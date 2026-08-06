41374cb3e79337217fea397c550371cd1ddaf10e
Youssif Abdelaziz <76822504+Youssif-AB@users.noreply.github.com>
2026-08-05 22:39:04 -0600

    Time: 2 ms (8.00%) | Memory: 12.2 MB (99.53%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,10 +4,8 @@ class Solution(object):
         finallist = []
         for i in digits:
             num += str(i)
-        
-        num = str(int(num) + 1)
 
-        for i in num:
+        for i in str(int(num) + 1):
             finallist.append(int(i))
         
         return finallist
