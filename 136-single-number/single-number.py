3720883dc1157a5ca65417dc2b74fca0ac0b2960
Youssif Abdelaziz <76822504+Youssif-AB@users.noreply.github.com>
2026-08-09 23:58:18 -0600

    Time: 9 ms (29.02%) | Memory: 15.2 MB (5.90%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        counter = {}
+        for i in nums:
+            if i in counter:
+                counter[i] += 1
+            else:
+                counter[i] = 1
+        for key, value in counter.items():
+            if value == 1:
+                return key
+        
